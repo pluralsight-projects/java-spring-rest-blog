110954bdc864ce24020623086a47a87daf6e5c73
Michael Broniola <mbroniola@gmail.com>
2020-12-13 09:10:03 -0800

    Module 3 updates

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -11,6 +11,8 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @RestResource(rel="contains-title", path="containsTitle")
     public List<Post> findByTitleContaining(String title);
 
+    public List<Post> findByAuthor_Lastname(String lastname);
+
 }
 
 /*
