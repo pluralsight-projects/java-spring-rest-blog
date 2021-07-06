0b2650c3acec06ba61746e6e643f640d71049981
azadusmans <azad.usman@cerner.com>
2021-07-06 06:12:05 +0530

    updated with session 3

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -18,4 +18,6 @@ public interface PostRepository  extends JpaRepository<Post, Long>{
     @RestResource(rel="contains-title", path="containsTitle")
     public List<Post> findByTitleContaining(String title);
 
+    public List<Post> findByAuthor_Lastname(String lastname);
+
 }
